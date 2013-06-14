d167b6c942b29cfb686311400665ba853ad4a66c
Renan T. Carneiro <renantc@uspgamedev.org>
2013-06-14 16:36:27 -0300

    Esqueleto da abelha criado

---
+++ abelha.rb
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+def abelha t
+  while true #TODO: definir condição de parada
+    #monitor.abelhaRequestPote
+    #espera t
+    #monitor.adicionaMel #  <-- talvez. Acho que aqui seria melhor, mas podemos
+    # mudar o algoritmo do monitor agora?
+    #monitor.abelhaReleasePote
+  end
+end
+  
