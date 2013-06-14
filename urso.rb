ced39bfe9487146a3113babd83d663d613941fb0
Renan T. Carneiro <renantc@uspgamedev.org>
2013-06-14 16:36:44 -0300

    Esqueleto do urso criado

---
+++ urso.rb
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+def urso T
+  while true #TODO: definir condição de parada
+    #monitor.ursoRequestPote
+    #espera T/2
+    #anuncia meio pote
+    #espera T-T/2 #caso T impar nao ferrar
+    #monitor.esvaziaPote # <-- fica mais claro. Podemos mudar o algoritmo do
+    # monitor?
+    #monitor.releasePote
+  end
+end
