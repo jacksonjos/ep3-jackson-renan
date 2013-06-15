7c1dbdabbdd6794368fd1e4564b878d5f20c81ab
Renan T. Carneiro <renantc@uspgamedev.org>
2013-06-15 01:25:34 -0300

    Abelha usa novas formas de pote e monitor

--- abelha.rb
+++ abelha.rb
@@ -5,11 +5,11 @@ require "monitor.rb"
 def abelha t
   
   while true #TODO: definir condição de parada
-    Monitor.abelha_request
+    ControladorAcesso.abelha_request
     #espera t
-    #monitor.adicionaMel #  <-- talvez. Acho que aqui seria melhor, mas podemos
+    Pote.adiciona_mel #  <-- talvez. Acho que aqui seria melhor, mas podemos
     # mudar o algoritmo do monitor agora?
-    Monitor.abelha_free
+    ControladorAcesso.abelha_free
   end
 end
   
