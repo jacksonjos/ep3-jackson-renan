ba5d87bd5802b12216edcf8048920cecc034fe6a
Renan T. Carneiro <renantc@uspgamedev.org>
2013-06-15 01:02:50 -0300

    Metodos do monitor implementados no urso

--- urso.rb
+++ urso.rb
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 def urso T
   while true #TODO: definir condição de parada
-    #monitor.ursoRequestPote
+    Monitor.urso_request
     #espera T/2
     #anuncia meio pote
     #espera T-T/2 #caso T impar nao ferrar
     #monitor.esvaziaPote # <-- fica mais claro. Podemos mudar o algoritmo do
     # monitor?
-    #monitor.releasePote
+    Monitor.urso_free
   end
 end
