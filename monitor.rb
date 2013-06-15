3fbae98e0494b697a686b5d838fdf3b36c46d2c9
Renan T. Carneiro <renantc@uspgamedev.org>
2013-06-15 01:25:15 -0300

    Separamos pote do monitor, e renomeamos monitor.

--- monitor.rb
+++ monitor.rb
@@ -6,20 +6,40 @@ require "monitor"
 $infoAbelha = []
 $infoUrso = []
 
-class Pote
-  attr_accessor :mel
+class Pote < Monitor
+  attr_reader :mel
 
   def initialize h
     @capacidadePote = h
   end
 
-  def cheio?
-    mel == @capacidadePote
+  def adiciona_mel
+    synchronize do
+      mel += 1
+      if @pote.meio_cheio?
+        # se abelha->rodando é verdade, entao enchendo @pote, else, esperando vaga
+        avisaMeioCheio
+      end
+    end
   end
 
-  def meio_cheio?
+  def esvazia_pote
+    synchronize do
+      mel = 0
+    end
+  end
+
+  def cheio?
+    synchronize do
+      mel == @capacidadePote
+    end
+  end
+
+  
+  private: def meio_cheio?
     mel == (@capacidadePote / 2).floor
   end
+
 end
 
 
@@ -37,7 +57,7 @@ def signal_all(condvar)
   condvar.broadcast
 end
 
-class Monitor
+class ControladorAcesso < Monitor
   attr_reader :numAbelhas
 
   
@@ -52,16 +72,10 @@ class Monitor
     
     synchronize do
       infoAbelha[i].estado = :voando     #
-      while !(numAbelhas < 100 && nenhumUrso && !@pote.cheio)
+      while !(numAbelhas < 100 && nenhumUrso && !@pote.cheio) #TODO: pode ser que uma abelha entre quando o tiver n abelhas enchendo o pote e h - n de mel no pote. Arrumar.
         wait(@entraAbelha) #
       end
       infoAbelha[i].estado = :depositando #
-
-      @pote.mel += 1
-      if @pote.meio_cheio?
-        # se abelha->rodando é verdade, entao enchendo @pote, else, esperando vaga
-        avisaMeioCheio
-      end
       
       numAbelhas += 1
     end
@@ -96,7 +110,6 @@ class Monitor
   def urso_free
     
     synchronize do
-      @pote.mel = 0
       signal_all(@entraAbelha)
     end
   end 
