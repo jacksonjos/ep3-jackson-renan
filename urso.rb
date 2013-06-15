648c2640d21620a59c1618711e4efdec73d538b2
Renan T. Carneiro <renantc@uspgamedev.org>
2013-06-15 01:25:53 -0300

    Urso usa novas formas de pote e monitor

--- urso.rb
+++ urso.rb
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 def urso T
   while true #TODO: definir condição de parada
-    Monitor.urso_request
+    ControladorAcesso.urso_request
     #espera T/2
     #anuncia meio pote
     #espera T-T/2 #caso T impar nao ferrar
-    #monitor.esvaziaPote # <-- fica mais claro. Podemos mudar o algoritmo do
+    Pote.esvazia_pote # <-- fica mais claro. Podemos mudar o algoritmo do
     # monitor?
-    Monitor.urso_free
+    ControladorAcesso.urso_free
   end
 end
