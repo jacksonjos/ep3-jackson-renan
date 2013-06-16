d8e42f52593da99d7db821e1625e1324cea570ea
Renan T. Carneiro <renanteruoc@gmail.com>
2013-06-16 18:36:41 -0300

    Gerenciador aogra orientado a eventos. Ainda porco.

--- timemanager.rb
+++ timemanager.rb
@@ -2,16 +2,38 @@
 class TimeManager
   
   skippedTime = 0
-  
+  pq = PriorityQueue.new
+
   def init
     startTime = curSystemTime
+    Thread.new{
+      sleep(dt)
+      while pq.minRank <= curTime
+        wake pq.remove
+      end
+    }
   end
   
+  def skipAteEvento
+    skipTime pq.minRank - curTime
+  end
+
+  def adicionaEvento t, rank
+    pq.adiciona t, rank
+  end
+
   def curTime
     return curSystemTime - startTime + skippedTime
   end
   
   def skipTime t
     skippedTime += t
+  end
+  
+  def encerra
+    while !pq.empty
+      wake pq.remove
+    end
+    #mata a thread que ele cria?
   end
 end
