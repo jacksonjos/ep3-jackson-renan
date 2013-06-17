de3151771f76b6ea692cd0a17517d54a75053efb
Renan T. Carneiro <renanteruoc@gmail.com>
2013-06-17 01:04:11 -0300

    Ex-comentarios de info abelha movidos para abelha

--- abelha.rb
+++ abelha.rb
@@ -6,6 +6,15 @@ require "timemanager.rb"
 
 class Abelha
   
+  # attr_accessor é um método do ruby que cria métodos setter e getter
+  # pras variáveis que são escritas logo depois dele antes de ':'.
+  # Assim, quando se quiser manipular a variável estado de um objeto
+  # 'Abelha' basta fazer abelha.estado = estado sendo abelha
+  # um objeto do tipo Abelha. No caso abaixo as variáveis definidas
+  # como attr_accessor são variáveis de instância, ou seja, :estado é 
+  # interpretado como @estado dentro de Abelha que é como se define
+  # uma variável de instância.
+
   attr_acessor :estado
 
   def initialize t, id
