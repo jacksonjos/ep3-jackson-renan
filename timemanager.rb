70b547b358a505a428056a24f13fed852fc67941
Renan T. Carneiro <renanteruoc@gmail.com>
2013-06-16 23:13:43 -0300

    timemanager monitorizado. Agora tambem lida com abelhas e ursos, falta saber quando pode skipar no caso das abelhas

--- timemanager.rb
+++ timemanager.rb
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
 require 'rubygems'
 require 'time'
 require 'priority_queue'
+require 'monitor'
+
+require 'monitoraliases.rb'
 
 # Neste EP cada unidade de tempo t gasta pelas das threads para realizar
 # as tarefas que lhes cabem equivale a um segundo
@@ -11,48 +15,83 @@ require 'priority_queue'
 
 # O método to_i devolve o valor inteiro de uma variável/objeto.
 
-class TimeManager
+class TimeManager < Monitor
   
   @skippedTime = 0
   @pq = PriorityQueue.new
+  @numAbelhas = 0
 
-  def init
+  @signalAbelhas = []
+
+  def init N
     # Time.new inicializa uma variável com o tempo corrente
     @startTime = Time.new
     # Como a unidade de tempo implementada foi de 1 segundo o sleep é implementado
     # para que a thread faça a checagem da fila uma vez por segundo. Afinal, quando
     # há diferença entre a prioridade (tempo) entre os objetos ela será medida entre
     # segundos.
-    Thread.new{
 
-      sleep(1)
-      while @pq.min_priority <= Time.now
-        wake @pq.pop
+    1..N.each{
+      @signalAbelhas << new_cond
+    }
+    
+    @totalAbelhas = N
+
+    @manager = Thread.new{
+
+      while true
+        sleep(1)
+        synchronize do
+          while @pq.min_priority <= current_time
+            signal @pq.pop
+          end
+        end
       end
     }
-  end
-  
-  def skip_ate_evento
-    @pq.min_priority - Time.now
-  end
-
-  def adiciona_evento t, priority
-    @pq.push t, t.to_i
-  end
-
-  def current_time
-    currentTime = Time.new - @startTime + @skippedTime
+    end
+    
+    # Retorna o tempo simbólico atual
+    def current_time
+    currentTime = Time.now - @startTime + @skippedTime
     return currentTime
   end
   
-  def skip_time t
-    @skippedTime += t
+  def espera_abelha id, t
+    synchronize do
+      @numAbelhas += 1
+      adiciona_evento @signalAbelhas[id], current_time + t
+      if false #TODO: alterar para verificar se nao vai entrar mais abelhas
+        skip_ate_evento
+      end
+      wait @signalAbelhas[id]
+      @numAbelhas -= 1
+    end
+  end
+
+  def espera_urso t
+    synchronize do
+      skip_time t
+    end
   end
   
   def encerra
     while !@pq.empty?
       wake @pq.pop
     end
-    #mata a thread que ele cria?
+    Thread.kill @manager
   end
+  
+  private
+
+  def skip_time t
+    @skippedTime += t
+  end
+
+  def skip_ate_evento
+    skip_time @pq.min_priority - current_time
+  end
+  
+  def adiciona_evento e, priority
+    @pq.push e, priority.to_i
+  
 end
